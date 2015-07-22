785e850b51aa71d96f4f537a1f01a7c326f6d6b2
ekanshmittal <ontoekansh@yahoo.com>
2015-07-22 16:31:43 +0530

    Generate all numbers with given number of digits

--- test/src/odometer/Odometer.java
+++ test/src/odometer/Odometer.java
@@ -1,6 +1,39 @@
 package odometer;
 
+import java.awt.font.NumericShaper;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Odometer {
+	private static final int START_DIGIT = 1;
+	private static final int LAST_DIGIT = 9;
+	private ArrayList<Integer> listOfNumbers;
+	private char[] number;
+
+	public ArrayList<Integer> findNumbers(int numberOfDigits) {
+		number = new char[numberOfDigits];
+		listOfNumbers = new ArrayList<Integer>();
+		generateNumbers(START_DIGIT, 0, numberOfDigits);
+		return listOfNumbers;
+	}
+
+	private void generateNumbers(int startNumber, int index, int numberOfDigits) {
+		if (numberOfDigits == 0) {
+			listOfNumbers.add(convertCharacterArrayToInteger(number));
+			return;
+		}
+		for (int i = startNumber; i <= LAST_DIGIT; i++) {
+			number[index] = convertIntegerToCharacter(i);
+			generateNumbers(i + 1, index + 1, numberOfDigits - 1);
+		}
+	}
+
+	private Integer convertCharacterArrayToInteger(char[] number) {
+		return Integer.parseInt(String.valueOf(number));
+	}
+
+	private Character convertIntegerToCharacter(Integer num) {
+		return (char) (num + '0');
+	}
 
 }
